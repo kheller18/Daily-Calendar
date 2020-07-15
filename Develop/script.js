15669e368409a67c416490c028587ba9eb1d7ff3
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-07-14 18:27:02 -0700

    created javascript function getting the current date and time of user

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,36 @@
+/*
+    function to get users time-zone
+        done
+
+    function to get current date and time
+        store date and print to top of page
+        store time
+
+    function to get calendar input values from local memory
+        store values to an array
+
+    function to display correct colors
+        get time from other function
+            display gray background for past
+            display red background for current
+            display green background for future
+
+    function to save values and store them in local memory
+*/
+
+//console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+
+
 
 $(document).ready(function() {
+    let date = moment().format("MMMM Do YYYY");
+    let time = moment().format("h:mm:ss a");
 
+    function getTime() {
+        console.log(time);
+        console.log(date);
+        $("#currentDay").text(date);
+    }
+    
+    getTime();
 });
