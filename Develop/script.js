f8c57d777bef95c64b4790b1cc66b00ec34ac07d
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-07-15 15:20:13 -0700

    stores and gets events for each time slot correctly

--- Develop/script.js
+++ Develop/script.js
@@ -25,18 +25,26 @@ $(document).ready(function() {
     let time = moment().format("h:mm:ss a");
     let hours = moment().format("HH");
     let event;
+    let events = [{"9": ""}, {"10": ""}, {"11": ""}, {"12": ""}, {"13": ""}, {"14": ""}, {"15": ""}, {"16": ""}, {"17": ""}];
+    let eventsC = [{"9": "", "10": "", "11": "", "12": "", "13": "", "14": "", "15": "", "16": "", "17": ""}];
+    
+    //console.log(eventsC);
     let correlation = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    const eventIds = ["#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16", "#17"];
+    //console.log(eventIds)
     var save;
+    var saveString;
+    
 
-    // gets current time in user timezone
     function getTime() {
-        console.log(time);
-        console.log(date);
-        console.log(hours)
+        //console.log(time);
+        //console.log(date);
+        //console.log(hours)
         $("#currentDay").text(date);
     }
 
     function setBackground() {
+        getEvents();
         getTime();
         if (correlation.includes(hours)) {
             event = $(".event").val(hours);
@@ -44,16 +52,52 @@ $(document).ready(function() {
             console.log("hey");
         } else {
             $(".event").addClass("past");
-            console.log("yo");
+            //console.log(event + "hi");
         }
     }
 
     // handles submit
     $(".saveBtn").on("click", function() {
-        // get text inside textbox
+        var i;
+        //window.localStorage.setItem("events", JSON.stringify(events));
         save = $(this).val();
+        i = eventIds[save - 9]; //
+        event = $(i).val();
         console.log(save);
-    })
+        console.log(event);
+        //console.log(i);
+        if (JSON.parse(localStorage.getItem("events")) != null) {
+            events = JSON.parse(localStorage.getItem("events"));
+        }
+        events[save - 9] = event;
+        console.log(events);
+        
+        window.localStorage.setItem("events", JSON.stringify(events));
+    });
+
+    function getEvents() {
+        var get = [];
+        if (JSON.parse(localStorage.getItem("events")) != null) {
+            get = JSON.parse(localStorage.getItem("events"));
+        }
+        console.log(get);
+        for (i = 0; i < get.length; i++) {// change from get to something else
+
+            if (get[i] == null) {
+                console.log("hello");
+            } else if (typeof get[i] === 'object') {
+                get[i] = "";
+            } else {
+                console.log(get[i]);
+            }
+
+            $(eventIds[i]).val(get[i]);
+            //console.log(eventIds[i]);
+            //console.log(get[i]);
+        }
+        console.log(get.length);
+        console.log(get);
+    }
 
 
     setBackground();
