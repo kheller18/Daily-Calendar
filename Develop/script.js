975d442357c2691a90013f7e7708b81932dbae0a
Keenan Heller <keenanheller@MacBook-Pro.local>
2020-07-15 18:46:45 -0700

    added more to functions to properly run and update based on time.

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,46 @@
 // only load javascript once HTML has been loaded correctly
 $(document).ready(function() {
+
+    // declaring initial variables
     let date = moment().format("MMMM Do YYYY");
-    let time = moment().format("hh:mm:ss");
+    let time = moment().format("hh:mm:ss a");
     let hours = moment().format("HH");
+    let minutes = moment().format("mm:ss");
     let event;
     let events = [{"9": ""}, {"10": ""}, {"11": ""}, {"12": ""}, {"13": ""}, {"14": ""}, {"15": ""}, {"16": ""}, {"17": ""}];
-    let eventsC = [{"9": "", "10": "", "11": "", "12": "", "13": "", "14": "", "15": "", "16": "", "17": ""}];
     var index;
     let correlation = [9, 10, 11, 12, 13, 14, 15, 16, 17];
     const eventIds = ["#9", "#10", "#11", "#12", "#13", "#14", "#15", "#16", "#17"];
     var save;
-    var saveString;
-    
 
+    // gets the time at the users location and calls related functions based on time of day
     function getTime() {
-        //console.log(time);
-        //console.log(date);
-        //console.log(hours);
-        if (time === ) 
+        window.setInterval(function() {
+            time = moment().format("hh:mm:ss a");
+            minutes = moment().format("mm:ss");
+            console.log(minutes);
+            if (time == "00:00:00 am") {
+                $("#currentDay").text(date);
+                localStorage.removeItem("events");
+                getEvents();
+            } 
+            if (minutes == "00:00") {
+                $(".event").removeClass("past present future")
+                setBackground();
+            }
+        }, 1000);
         $("#currentDay").text(date);
+        setBackground();
     }
 
+    // sets the color of the background color based on the current time at users location
     function setBackground() {
-        var z;
+        hours = moment().format("HH");
+        console.log(hours);
         getEvents();
-        getTime();
 
         if (correlation.includes(parseInt(hours))) {
+            console.log('wrong')
             index = correlation.indexOf(parseInt(hours));
             for (z = 0; z < index; z++) {
                 $(eventIds[z]).addClass("past");
@@ -40,50 +54,37 @@ $(document).ready(function() {
         }
     }
 
-    // handles submit
+    // handles user saving an event and saves it to local storage for that respective timeslot
     $(".saveBtn").on("click", function() {
-        var i;
-        //window.localStorage.setItem("events", JSON.stringify(events));
         save = $(this).val();
-        i = eventIds[save - 9]; //
-        event = $(i).val();
-        console.log(save);
-        console.log(event);
-        //console.log(i);
+        event = $(eventIds[save - 9]).val().trim();
+
         if (JSON.parse(localStorage.getItem("events")) != null) {
             events = JSON.parse(localStorage.getItem("events"));
         }
+
         events[save - 9] = event;
-        console.log(events);
-        
         window.localStorage.setItem("events", JSON.stringify(events));
     });
 
+    // gets events from local storage and displays them in each respective timeslot
     function getEvents() {
         var get = [];
+
         if (JSON.parse(localStorage.getItem("events")) != null) {
             get = JSON.parse(localStorage.getItem("events"));
+        } else {
+            $(".event").val('');
         }
-        //console.log(get);
-        for (i = 0; i < get.length; i++) {// change from get to something else
 
-            if (get[i] == null) {
-                console.log("hello");
-            } else if (typeof get[i] === 'object') {
+        for (i = 0; i < get.length; i++) {
+            if (get[i] === null || typeof get[i] === 'object') {
                 get[i] = "";
-            } else {
-                //console.log(get[i]);
             }
-
             $(eventIds[i]).val(get[i]);
-            //console.log(eventIds[i]);
-            //console.log(get[i]);
         }
-        //console.log(get.length);
-        //console.log(get);
     }
 
-
-    setBackground();
-  //  getTime();
+    // kicks off the application
+    getTime();
 });
